0b13f65268b231d60feb231018a2cfa833c1bb73
Boris Stepanenko <silkroad1457@mail.ru>
2012-02-10 22:23:33 +0400

    Removed map_min and map_max from documentation

--- main.cpp
+++ main.cpp
@@ -1,14 +1,26 @@
 #include <stdio.h>
 #include "map.h"
 
+typedef struct {
+	int a;
+	int b;
+} myStruct_t;
+
 int main(void) {
-	Map map = map_allocate(2);
-	map_insert(map, "0", "world");
-	map_insert(map, "1", "world1");
-	map_insert(map, "2", "world2");
-	printf("%s\n", (char*)map_find(map, "0"));
-	printf("%s\n", (char*)map_find(map, "1"));
-	printf("%s\n", (char*)map_find(map, "2"));
+	myStruct_t foo;
+	foo.a = 0;
+	foo.b = 1;
+	myStruct_t bar;
+	bar.a = 2;
+	bar.b = 3;
+
+	Map map = map_allocate(1024);
+	map_insert(map, "foo", &foo);
+	map_insert(map, "bar", &bar);
+	myStruct_t* foo_ = (myStruct_t*)map_find(map, "foo");
+	myStruct_t* bar_ = (myStruct_t*)map_find(map, "bar");
+	printf("%d %d\n", foo_->a, foo_->b);
+	printf("%d %d\n", bar_->a, bar_->b);
 	map_free(map);
 	printf("Normal exit\n");
 	return(0);
